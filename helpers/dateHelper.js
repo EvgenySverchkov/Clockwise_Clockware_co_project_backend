57796f07d8bb793fdd488886cb46d065c95ed3cf
EvgenySverchkov <47426436+EvgenySverchkov@users.noreply.github.com>
2020-10-03 10:14:54 +0300

    fixed bug

--- helpers/dateHelper.js
+++ helpers/dateHelper.js
@@ -1,7 +1,7 @@
 exports.isClientDateLargeThenCurrDate = function (clientDate) {
   const clientDt = new Date(clientDate);
   const currDate = new Date();
-
+  currDate.setDate(currDate.getDate()-1);
   if (currDate.getTime() > clientDt.getTime()) {
     return false;
   } else {
